ebb1bedb6448dd69c5894cf139bad5dbfd47d592
dennisko762 <dennis.korolevych@campus.tu-berlin.de>
2021-04-30 23:45:00 +0200

    Merge branch 'main' of https://github.com/dennisko762/budget-app into testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
+import { BudgetItemComponent } from './budget-item/budget-item.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    BudgetItemComponent,
+    BudgetItemListComponent,
+    AddItemFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
